cbf6a5afd2922a32356a9625b8014f383ebc8f27
SamagandiMahesh <mahesh.samagandi89@gmail.com>
2023-08-01 13:50:08 +0530

    Loader logic added

--- apps/snaptrip/src/app/app.tsx
+++ apps/snaptrip/src/app/app.tsx
@@ -124,6 +124,7 @@ const StyledApp = styled.div`
   .card-container {
     margin: 20px;
     padding: 20px;
+    position: relative;
   }
 
   .card-container:hover {
@@ -181,6 +182,27 @@ const StyledApp = styled.div`
   .travel-option-details {
     width: 100%;
   }
+
+  .loader-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+  }
+  
+  .loader {
+    color: #fff;
+    font-size: 24px;
+    font-weight: bold;
+    /* Add your custom styles for the loader */
+  }
+
 `
 export function App() {
 
--- apps/snaptrip/src/app/organisms/TripForm.tsx
+++ apps/snaptrip/src/app/organisms/TripForm.tsx
@@ -1,206 +1,227 @@
 import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import {
-    Button,
-    FormControl,
-    Container,
-    Card,
-    Box,
-    FormLabel,
-    Grid,
-    InputLabel,
-    MenuItem,
-    Select,
-    Slider,
-    TextField} from '@material-ui/core';
+  Button,
+  FormControl,
+  Container,
+  Card,
+  Box,
+  FormLabel,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Slider,
+  TextField
+} from '@material-ui/core';
 import ErrorBoundary from '../components/ErrorBoundary';
 import FileInput from '../components/FileInput';
 import { cityList, marks } from '../mocks/mock';
 
 interface ChildComponentProps {
-    handleViewChange: (data: any) => void;
-  }
+  handleViewChange: (data: any) => void;
+}
 
 const TripForm: React.FC<ChildComponentProps> = ({ handleViewChange }) => {
-    const {
-        control, // use "control" from useForm to initialize the controller
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
- 
-    const getLocation = () => {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              const { latitude, longitude } = position.coords;
-            //   setCurrentLocation(`${latitude},${longitude}`);
-            },
-            (error) => {
-              console.error('Error getting location:', error);
-            }
-          );
+  const {
+    control, // use "control" from useForm to initialize the controller
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [loading, setLoading] = useState(false);
+
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          //   setCurrentLocation(`${latitude},${longitude}`);
+        },
+        (error) => {
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  };
+
+  const handleSubmitWithLoader = (data: Record<string, any>) => {
+    setLoading(true); // Show the loader
+
+
+    // Hide the loader after 40 seconds
+    setTimeout(() => {
+      setLoading(false);
+      handleViewChange(data);
+    }, 10000);
+  };
+
+  const onSubmit = (data: Record<string, any>) => {
+    // setLoading(true);
+    const formData = new FormData();
+    if (data.image) {
+      formData.append('inputImage', data.image[0]);
+    }
+    // const sourceLocation = cityList[Math.floor(Math.random() * cityList.length)];
+    const sourceLocation = 'kolkata';
+    formData.append('sourceLocation', sourceLocation);
+    formData.append('duration', data.duration);
+    formData.append('budget', data.budget);
+    formData.append('budgetCurrency', data.budgetCurrency);
+    handleSubmitWithLoader(data);
+    return fetch("http://127.0.0.1:5000/snaptrip", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json(); // Parse the response body as JSON and return it
         } else {
-          console.error('Geolocation is not supported by this browser.');
+          throw new Error("Error fetching data from the SnapTrip API.");
         }
-      };
+      })
+      .then((data) => {
+        // Handle successful response data here
+        handleViewChange(data);
+      })
+      .catch((error) => {
+        // Handle error
+        console.log("Error fetching data:", error.message);
+      })
+      .finally(() => {
+        // setLoading(false); // Stop the loading state regardless of success or error
+      });
 
-    const onSubmit = (data: Record<string, any>) => {
-        const formData = new FormData();
-        if (data.image) {
-            formData.append('inputImage', data.image[0]);
-        }
-        // const sourceLocation = cityList[Math.floor(Math.random() * cityList.length)];
-        const sourceLocation = 'kolkata';
-        formData.append('sourceLocation', sourceLocation);
-        formData.append('duration', data.duration);
-        formData.append('budget', data.budget);
-        formData.append('budgetCurrency', data.budgetCurrency);
-        //handleViewChange(data);
-        return fetch("http://127.0.0.1:5000/snaptrip", {
-          method: "POST",
-          body: formData,
-        })
-          .then((response) => {
-            if (response.ok) {
-              return response.json(); // Parse the response body as JSON and return it
-            } else {
-              throw new Error("Error fetching data from the SnapTrip API.");
-            }
-          })
-          .then((data) => {
-            // Handle successful response data here
-            handleViewChange(data);
-          })
-          .catch((error) => {
-            // Handle error
-            console.log("Error fetching data:", error.message);
-          });
-          
-    };
+  };
 
-    return (
-        <Container maxWidth="sm">
-            <Card variant="outlined" className='card-container'>
-            <ErrorBoundary>
-            
-                <form onSubmit={handleSubmit(onSubmit)}>
+  return (
+    <Container maxWidth="sm">
+      <Card variant="outlined" className='card-container'>
+        <ErrorBoundary>
 
-                    <FormControl fullWidth error={Boolean(errors.image)} margin="normal">
-                        {/* Use Controller for input type="file" */}
+          <form onSubmit={handleSubmit(onSubmit)}>
 
-                        {/* <FormLabel className='margin-10' >Upload the Snap</FormLabel> */}
-                        <FileInput control={control} />
+            <FormControl fullWidth error={Boolean(errors.image)} margin="normal">
+              {/* Use Controller for input type="file" */}
 
-                        {/* {errors.image?.message && (
+              {/* <FormLabel className='margin-10' >Upload the Snap</FormLabel> */}
+              <FileInput control={control} />
+
+              {/* {errors.image?.message && (
                 <FormHelperText>{errors.image.message}</FormHelperText>
               )} */}
-                    </FormControl>
+            </FormControl>
 
-                    <FormControl fullWidth error={Boolean(errors.duration)} margin="normal">
-                        {/* Use Controller for Slider */}
-                        <FormLabel>Duration</FormLabel>
-                        <Controller
-                            name="duration"
-                            control={control}
-                            defaultValue={1}
-                            rules={{
-                                required: 'Duration is required',
-                                min: { value: 1, message: 'Duration should be at least 1' },
-                                max: { value: 15, message: 'Duration should be at most 15' },
-                            }}
-                            render={({ field }) => (
-                                <>
-                                    <Slider
-                                        id="duration"
-                                        name="duration"
-                                        value={field.value}
-                                        onChange={(_, value) => field.onChange(value)}
-                                        defaultValue={1}
-                                        step={1}
-                                        marks={marks}
-                                        min={1}
-                                        max={15}
-                                        valueLabelDisplay="auto"
-                                    />
-                                </>
-                            )}
-                        />
-                        {/* <FormHelperText>{errors.duration?.message}</FormHelperText> */}
-                    </FormControl>
+            <FormControl fullWidth error={Boolean(errors.duration)} margin="normal">
+              {/* Use Controller for Slider */}
+              <FormLabel>Duration</FormLabel>
+              <Controller
+                name="duration"
+                control={control}
+                defaultValue={1}
+                rules={{
+                  required: 'Duration is required',
+                  min: { value: 1, message: 'Duration should be at least 1' },
+                  max: { value: 15, message: 'Duration should be at most 15' },
+                }}
+                render={({ field }) => (
+                  <>
+                    <Slider
+                      id="duration"
+                      name="duration"
+                      value={field.value}
+                      onChange={(_, value) => field.onChange(value)}
+                      defaultValue={1}
+                      step={1}
+                      marks={marks}
+                      min={1}
+                      max={15}
+                      valueLabelDisplay="auto"
+                    />
+                  </>
+                )}
+              />
+              {/* <FormHelperText>{errors.duration?.message}</FormHelperText> */}
+            </FormControl>
 
-                    <Box sx={{ flexGrow: 1 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} md={6}>
-                                <FormControl fullWidth error={Boolean(errors.budget)} margin="normal">
-                                    {/* Use Controller for TextField */}
-                                    <Controller
-                                        name="budget"
-                                        control={control}
-                                        defaultValue=""
-                                        rules={{ required: 'Budget is required' }}
-                                        render={({ field }) => (
-                                            <>
-                                                <TextField
-                                                    id="budget"
-                                                    name="budget"
-                                                    type="number"
-                                                    value={field.value}
-                                                    onChange={(e) => field.onChange(e.target.value)}
-                                                    label="Budget"
-                                                    variant="outlined"
-                                                />
-                                            </>
-                                        )}
-                                    />
-                                    {/* {errors.budget?.message && (
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <FormControl fullWidth error={Boolean(errors.budget)} margin="normal">
+                    {/* Use Controller for TextField */}
+                    <Controller
+                      name="budget"
+                      control={control}
+                      defaultValue=""
+                      rules={{ required: 'Budget is required' }}
+                      render={({ field }) => (
+                        <>
+                          <TextField
+                            id="budget"
+                            name="budget"
+                            type="number"
+                            value={field.value}
+                            onChange={(e) => field.onChange(e.target.value)}
+                            label="Budget"
+                            variant="outlined"
+                          />
+                        </>
+                      )}
+                    />
+                    {/* {errors.budget?.message && (
                 <FormHelperText>{errors.budget.message}</FormHelperText>
               )} */}
-                                </FormControl>
-                            </Grid>
-                            <Grid item xs={12} md={6}>
-                                <FormControl fullWidth margin="normal" error={Boolean(errors.budgetCurrency)} variant='outlined'>
-                                    <InputLabel id="budget-currency-label">Budget Currency</InputLabel>
-                                    {/* Use Controller for Select */}
-                                    <Controller
-                                        name="budgetCurrency"
-                                        control={control}
-                                        defaultValue="usd"
-                                        render={({ field }) => (
-                                            <>
-                                                <Select
-                                                    value={field.value}
-                                                    onChange={(e) => field.onChange(e.target.value)}
-                                                    labelId="budget-currency-label"
-                                                    id="budget-currency-label"
-                                                    label="Budget Currency"
-                                                >
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <FormControl fullWidth margin="normal" error={Boolean(errors.budgetCurrency)} variant='outlined'>
+                    <InputLabel id="budget-currency-label">Budget Currency</InputLabel>
+                    {/* Use Controller for Select */}
+                    <Controller
+                      name="budgetCurrency"
+                      control={control}
+                      defaultValue="usd"
+                      render={({ field }) => (
+                        <>
+                          <Select
+                            value={field.value}
+                            onChange={(e) => field.onChange(e.target.value)}
+                            labelId="budget-currency-label"
+                            id="budget-currency-label"
+                            label="Budget Currency"
+                          >
 
-                                                    <MenuItem value="usd">USD</MenuItem>
-                                                    <MenuItem value="eur">EUR</MenuItem>
-                                                    <MenuItem value="gbp">GBP</MenuItem>
-                                                    <MenuItem value="inr">INR</MenuItem>
-                                                </Select>
-                                            </>
-                                        )}
-                                    />
-                                </FormControl>
-                            </Grid>
-                        </Grid>
-                    </Box>
+                            <MenuItem value="usd">USD</MenuItem>
+                            <MenuItem value="eur">EUR</MenuItem>
+                            <MenuItem value="gbp">GBP</MenuItem>
+                            <MenuItem value="inr">INR</MenuItem>
+                          </Select>
+                        </>
+                      )}
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </Box>
+
+            <Button type="submit" variant="contained" color="primary" className='submit-button'>
+              Get Itinerary
+            </Button>
+
+            {loading && ( // Render the loader overlay when loading is true
+              <div className="loader-overlay">
+                <div className="loader">Loading...</div>
+              </div>
+            )}
+          </form>
 
 
-
-                    
-                    <Button type="submit" variant="contained" color="primary" className='submit-button'>
-                        Get Itinerary
-                    </Button>
-
-                </form>
-               
-            </ErrorBoundary >
-            </Card>
-        </Container>
-    );
+        </ErrorBoundary >
+      </Card>
+    </Container>
+  );
 };
 
 export default TripForm;
